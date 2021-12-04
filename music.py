c170d5837748a295553d1ec66edbb2c34670c73a
dfoulks <dfoulks@apache.org>
2021-12-04 16:09:39 -0600

    condensed the music script

--- music.py
+++ music.py
@@ -2,42 +2,19 @@
 import gpiozero
 import time
 import sys
+
 button = gpiozero.Button(12)
 b = gpiozero.TonalBuzzer(17)
 tone = gpiozero.tones.Tone
-def pachabel():
-    b.play(tone("D5"))
-    time.sleep(1)
-    b.stop()
 
-    b.play(tone("A4"))
-    time.sleep(1)
-    b.stop()
+song = [ "D5", "A4", "B4", "F#4", "G4", "D4", "G4", "A4"]
 
-    b.play(tone("B4"))
-    time.sleep(1)
-    b.stop()
-
-    b.play(tone("F#4"))
-    time.sleep(1)
-    b.stop()
-
-    b.play(tone("G4"))
-    time.sleep(1)
-    b.stop()
-
-    b.play(tone("D4"))
-    time.sleep(1)
-    b.stop()
-
-    b.play(tone("G4"))
-    time.sleep(1)
-    b.stop()
-
-    b.play(tone("A4"))
-    time.sleep(1)
-    b.stop()
+def play(tones):
+    for t in tones:
+        b.play(tone(t))
+        time.sleep(1)
+        b.stop()
 
 while True:
     if button.is_active is True:
-        pachabel()
+        play(song)
